eff989e1ffd76ce4ea7984e7c3b0de41a2d8bde5
Casperfeld <casper@feldmann.ca>
2026-04-20 13:38:50 -0700

    Delete DFQuestionnaireV1 directory

--- DFQuestionnaireV1/Models/ValueQuestion.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace DFQuestionnaireV1.Models
-{
-    public class ValueQuestion
-    {
-        public string Text { get; set; } = string.Empty;
-        public int Answer { get; set; }
-    }
-}
